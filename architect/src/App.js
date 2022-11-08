be7efeea235a2f5a2afafecc90c3d2acea4294e1
Davit Martirosyan <davo.amrtirosyan.96@bk.ru>
2022-11-09 00:54:02 +0400

    feat: integration of esLint and prettier configs

--- architect/src/App.js
+++ architect/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello Java script</p>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<p>Hello Java script</p>
+		</div>
+	);
 }
 
 export default App;
