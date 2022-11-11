055daa61fd53674546ad4843e324bfe393258451
Davit Martirosyan <davo.amrtirosyan.96@bk.ru>
2022-11-12 01:50:51 +0400

    add: sass, propTypes, remove: Some files that are not needed

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+import Header from '../layouts/header';
+import styles from './index.module.scss';
+
+function App() {
+	return (
+		<div className={styles.App}>
+			<Header />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/layouts/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function Header() {
+	return (
+		<div>
+			<h1>Hello World</h1>
+		</div>
+	);
+}
+
+// Header.PropTypes = {};
+
+export default Header;
